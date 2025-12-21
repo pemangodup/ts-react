0c8ea23e0054a9478fb4fd4205fe6f3e9132ead2
Pema Ngodup <pngodup123@gmail.com>
2025-12-21 09:24:39 +0000

    Form on sumbit submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,21 @@
-import { ReactNode, type ComponentPropsWithoutRef } from "react";
+import { FormEvent, type ComponentPropsWithoutRef } from "react";
 
-type FormProps = { children: ReactNode } & ComponentPropsWithoutRef<"form">;
+type FormProps = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
 
-export default function Form({ children, ...props }: FormProps) {
-  return <form {...props}>{children}</form>;
+export default function Form({ onSave, children, ...otherProps }: FormProps) {
+  function formHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+  }
+
+  return (
+    <form onSubmit={formHandler} {...otherProps}>
+      {children}
+    </form>
+  );
 }
