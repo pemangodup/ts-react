42ef5f539082d2c24ff202a6d578378a6d5cf311
Pema Ngodup <pngodup123@gmail.com>
2025-12-22 22:34:15 +0000

    End of advance concept types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,48 @@
-import { FormEvent, type ComponentPropsWithoutRef } from "react";
+import {
+  FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("Clearing...");
+        form.current?.reset();
+      },
+    };
+  });
+
   function formHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData);
     onSave(data);
+    form.current?.reset();
   }
 
   return (
-    <form onSubmit={formHandler} {...otherProps}>
+    <form onSubmit={formHandler} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+});
+
+export default Form;
