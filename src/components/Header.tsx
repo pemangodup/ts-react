597bc337efa3374b3bf79484c3289d22cdd51378
Pema Ngodup <pngodup123@gmail.com>
2025-12-25 12:49:01 +0000

    Creating a custome hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import Button from "./UI/Button";
+import { useTimersContext } from "../store/timers-context";
 
 export default function Header() {
+  const timersCtx = useTimersContext();
   return (
     <header>
       <h1>ReactTimer</h1>
-      <Button el="button">Stop Timers</Button>
+      <Button el="button">
+        {timersCtx.isRunning ? "Stop" : "Start"} Timers
+      </Button>
     </header>
   );
 }
---
+++ src/store/timers-context.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, type ReactNode } from "react";
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContextValue = TimersState & {
+  addTimer: (data: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+  if (timersCtx === null) {
+    throw new Error("TimersContext is null - that should not be the case!");
+  }
+  return timersCtx;
+}
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    isRunning: false,
+    timers: [],
+    addTimer: (timerData) => {},
+    startTimers: () => {},
+    stopTimers: () => {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
